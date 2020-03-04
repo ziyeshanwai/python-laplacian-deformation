807b1955771ebdb28c8c1cc4807c079f1215f021
LiyouWang <1085066875@qq.com>
2020-03-04 16:45:38 +0800

    demo

--- Demo.py
+++ Demo.py
@@ -18,17 +18,21 @@ if __name__ == "__main__":
     #         print(nei.ID + 1)
     v = np.array(v)
     # v = np.array(v) - np.mean(v, axis=0)
+    to_move_points_index = 2531
     anchors_ids = load_pickle_file(r"./anchor_id.pkl")
     anchors_ids = np.array(anchors_ids, dtype=np.int32)
     anchors = v[anchors_ids, :]
     WEIGHT = np.ones((anchors.shape[0],), dtype=np.float32)
-    print("the origin index 2527 coordinates is {}".format(anchors[np.where(anchors_ids == 2527)[0], :]))
-    target_points = np.array([[-0.00964538, 0.15759308, 2.204274*1.2]], dtype=np.float32)
-    anchors[np.where(anchors_ids == 2527)[0], :] = target_points
-    print("the changed index 2527 is {}".format(target_points))
+    print("the origin index {} coordinates is {}".format(to_move_points_index, anchors[np.where(anchors_ids == to_move_points_index)[0], :]))
+    tmp = anchors[np.where(anchors_ids == to_move_points_index)[0], :]
+    tmp[:, 2] = tmp[:, 2] * 2.5
+    target_points = tmp
+    # target_points = np.array([[-0.00964538, 0.15759308, 2.204274*1.2]], dtype=np.float32)
+    anchors[np.where(anchors_ids == to_move_points_index)[0], :] = target_points
+    print("the changed index {} is {}".format(to_move_points_index, target_points))
     # anchors[np.where(anchors_ids == 2529)[0], :] = target_points
     # anchors[np.where(anchors_ids == 2531)[0], :] = target_points
     anchorsIdx = np.array(anchors_ids, dtype=np.int32)
     laplacian = LaplacianDeformation(polymesh, WEIGHT)
-    laplacian.solveLaplacianMesh(anchors, anchorsIdx, False)
+    laplacian.solveLaplacianMesh(anchors, anchorsIdx, True)
     laplacian.mesh.saveObjFile(r"./output.obj")
